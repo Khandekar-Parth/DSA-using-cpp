8ebf97fbc412b74ae269f99d9eb5a2adeb128adf
Khandekar-Parth <parthkhandekar1210@gmail.com>
2025-08-30 12:24:27 +0530

    fixing

--- loop.cpp
+++ loop.cpp
@@ -96,22 +96,3 @@
 // }
 
 #include <iostream>
-using namespace std;
-
-int main()
-{
-    int n = 5;
-    int sum = 0;
-
-    for (int i = 1; i <= n; i++)
-    {
-        if (i % 2 != 0)
-        {
-            sum = sum + i;
-        }
-
-        cout << endl;
-    }
-    cout << sum << "\n";
-    return 0;
-}
